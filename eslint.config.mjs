e5e4d59fa6fb515f3445e3068c1910922cc3cbf5
Evander Mendonca <evandermendonca@Evanders-MacBook-Pro.local>
2025-01-18 01:33:59 -0800

    Setup code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      semi: ['error'],
+      quotes: ['error', 'single'],
+      'semi-style': ['error', 'last'],
+      'max-len': ['error', {
+        'code': 80, // Maximum line length
+        'tabWidth': 2, // Number of spaces per tab
+        'ignoreComments': true, // Ignore comments when checking length
+        'ignoreTrailingComments': true, // Ignore trailing comments
+        'ignoreUrls': true, // Ignore URLs when checking length
+        'ignoreStrings': true, // Ignore strings when checking length
+        'ignoreTemplateLiterals': true, // Ignore template literals when checking length
+        'ignoreRegExpLiterals': true // Ignore regular expression literals when checking length
+      }]
+    },
+  })
 ];
 
 export default eslintConfig;
